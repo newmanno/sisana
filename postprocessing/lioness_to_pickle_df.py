9ed772307ea8813e29b72123d37399b23d3213be
newmanno <newmanno@oregonstate.edu>
2023-11-16 14:26:54 +0100

    spelling

--- postprocessing/lioness_to_pickle_df.py
+++ postprocessing/lioness_to_pickle_df.py
@@ -9,14 +9,14 @@ from post import files_to_dfs
 if __name__ == '__main__':
     """
     Description:
-        This code converts the output of the lioness file to a pickle format
+        This code converts the output of the lioness file to a pickle format and adds names of the samples to the df
     """
         
     parser = argparse.ArgumentParser(description="Example command: python lioness_to_pickle_df.py -p panda_output.txt -q lioness_output.npy -t npy -o ./output/lioness_df.pickle")
     requiredArgGroup = parser.add_argument_group('Required arguments')  
     requiredArgGroup.add_argument("-p", "--pandaFile", type=str, help="Path to panda output produced by the run_panda.py script", required=True) 
     requiredArgGroup.add_argument("-q", "--lionessFile", type=str, help="Path to file produced by the run_lioness.py script", required=True)
-    requiredArgGroup.add_argument("-t", "--loinessFileType", type=str, choices = ['txt', 'npy'], help="File type of lioness input (the -q file)", required=True)
+    requiredArgGroup.add_argument("-t", "--lionessFileType", type=str, choices = ['txt', 'npy'], help="File type of lioness input (the -q file)", required=True)
     requiredArgGroup.add_argument("-n", "--sampnames", type=str, help="File with list of sample names (one per line) in the same order that were supplied to run_lioness.py", required=True)    
     requiredArgGroup.add_argument("-o", "--outfile", default = "./lioness_output.pickle", type=str, help="Path to output file in pickle format (e.g. lioness.pickle)", required=True)    
     
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     
     print("Reading in data...")
 
-    dfs_from_files = files_to_dfs(args.pandaFile, args.lionessFile, args.loinessFileType)
+    dfs_from_files = files_to_dfs(args.pandaFile, args.lionessFile, args.lionessFileType)
 
     # Create data frames from input files
     pan = dfs_from_files[0]
