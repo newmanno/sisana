93af93351c0fe436ee1e6936705ab16e0f837221
newmanno <newmanno@oregonstate.edu>
2024-08-02 15:35:44 +0200

    Added log2 FC and modified mw test stat calculation

---
+++ network_analysis/analyze/compare.py
@@ -0,0 +1,144 @@
+import scipy 
+from scipy import stats
+import csv
+import re
+import pandas 
+import math
+
+def file_to_list(fname):
+    """
+    This function takes as input a text file with one object per line and returns the contents of that file as a list
+
+    Args:
+        fname: the name of the file to convert to a list
+
+    Returns:
+        returnlist: list of objects from the text file
+    """
+    with open(fname, 'r') as file:
+        returnlist = []
+
+        for line in file:
+            line = line.strip()
+            returnlist.append(line)    
+            
+        return (returnlist)   
+
+def map_samples(mapfile, type1, type2):
+    '''
+    Function that assigns samples to groups for statistical analysis
+
+        Arguments:
+            - mapfile: input file from user
+            - type1: the name of the group in the first set of samples, must be present in the mapfile
+            - type2: the name of the group in the second set of samples, must be present in the mapfile
+    '''
+
+    samp_type_dict = {}
+
+    type1_list = []
+    type2_list = []
+
+    init_dict = {}
+    samp_type_dict = {}
+
+    # Add all node-type pairs from the input file into the node_type_dict
+    with open(mapfile) as samp_file:
+        samp_file = csv.reader(samp_file, delimiter = ',')
+
+        for row in samp_file:
+            print(row)
+            init_dict[row[0]] = row[1]
+
+    for key,value in init_dict.items():
+        try:
+            if re.search(type1, value):
+                type1_list.append(key)
+            elif re.match(type2, value):
+                type2_list.append(key)
+        except:
+            print("Unexpected value found for the sample group name.")
+
+    samp_type_dict[type1] = type1_list
+    samp_type_dict[type2] = type2_list
+
+    return (samp_type_dict)
+
+def calc_tt(group1, group2, ttype):
+    '''
+    Performs either a students t-test or mann-whitney test between two groups
+    
+        Arguments:
+            - group1: list of samples in first group
+            - group2: list of samples in second group
+            - ttype: str, the type of test to perform, either tt or mw
+    '''
+    
+    if ttype == 'tt':
+        p = stats.ttest_ind(group1, group2)       
+        statistic = p[0]
+        pval = p[1]
+    elif ttype == 'mw':
+        p = stats.mannwhitneyu(group1, group2)
+        
+        # Note that since mann whitney technically has two test statistics (one for U1 and the other for U2), we have to compute both and take the minimum.
+        # Wilcoxon also has two test statistics, but by default it returns the smaller of the two, so no need for SiSaNA to calculateNo
+        u1 = p[0]
+        u2 = group1.shape[0] * group2.shape[0] - p[0]
+        
+        statistic = min(u1,u2)
+        pval = p[1]
+    elif ttype == 'paired_tt':
+        p = stats.ttest_rel(group1, group2)
+        statistic = p[0]
+        pval = p[1]
+    elif ttype == 'wilcoxon':
+        p = stats.wilcoxon(group1, group2)
+        statistic = p[0]
+        pval = p[1]
+
+    return(statistic, pval)
+
+def calc_log2_fc(group1, group2):
+    '''
+    Calculates the log2 fold change of means across two groups
+        
+        Arguments:
+            - group1: list of samples in first group
+            - group2: list of samples in second group
+    '''
+    
+    # print("\n")
+    # print(f"group1 values:")
+    # print(group1)
+    # print(f"Mean of group 1: {scipy.mean(group1)}")
+    # print(f"group2 values:")
+    # print(group2)
+    # print(f"Mean of group 2: {scipy.mean(group2)}")
+
+    ### Note: For the following calculations, assuming the user has followed the previous SiSaNA steps,
+    ### neither group will ever have a mean of 0 since the preprocess.py step filters out any genes of 
+    ### low abundance. So to simplify the logic for this step, I have removed any checks for group means
+    ### of 0. May need to implement this change later though if people are not running preprocess.py.
+
+    # if scipy.mean(group1) == 0 and scipy.mean(group2) == 0:
+    #     log2FC = 0
+    # elif scipy.mean(group1) == 0:
+        
+    # elif scipy.mean(group2) != 0:
+    
+    # Only run if all values are positive, otherwise the user likely entered degree as their data file accidentally
+    if all(i > 0 for i in group1) and all(i > 0 for i in group2):
+        log2FC = math.log2(scipy.mean(group2)/scipy.mean(group1))
+    else:
+        print(group1, group2)
+        raise Exception("\n\nError: Negative values found in data. The log2 fold change can only be calculated on expression data. Negative values indicate that degree was likely used as an input type instead.\n")
+    # elif scipy.mean(group2) == 0:
+    #     # Take the smallest non-zero value, divide by 10, and use that as the average value for the denominator
+    #     no_zeros = [i for i in group1 if i != 0]
+    #     denom = min(no_zeros)/10
+    #     log2FC = math.log2(scipy.mean(group1)/denom)        
+    # else:
+    #     log2FC = "NA"
+    
+    return(log2FC)
