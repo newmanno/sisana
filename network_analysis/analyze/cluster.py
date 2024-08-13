89360ff9933973408007f3be7601150b5850476a
newmanno <newmanno@oregonstate.edu>
2024-08-13 16:02:26 +0200

    Added option for Hartigans method

--- network_analysis/analyze/cluster.py
+++ network_analysis/analyze/cluster.py
@@ -46,7 +46,7 @@ def pca_fit_transform(scaled, nc, df_index):
     return(pca_dict)
 
 
-def km(pca_df, kmax, numsamps):
+def km(pca_df, kmax, numsamps, clusttype):
     '''
     Function that performs kmeans clustering on data for multiple values of k and returns the silhouette scores for each
     
@@ -60,58 +60,85 @@ def km(pca_df, kmax, numsamps):
             The maximum value of k
         numsamps : int
             The number of samples in the data frame
+        clusttype : str [choices are kmeans or hkmeans]
+            The type of clustering to perform. kmeans performs standard kmeans clustering while hkmeans uses the Hartigan method 
     ''' 
-
+    
     silhouette_scores = []
 
-    kmeans_kwargs = {
-        "init": "random",
-        "n_init": 10,
-        "max_iter": 300,
-        "random_state": 42,
-    }
+    if clusttype == "kmeans":
+        kmeans_kwargs = {
+            "init": "random",
+            "n_init": 10,
+            "max_iter": 300,
+            "random_state": 42,
+        }
 
-    for k in range(2, kmax):
-        kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
-        kmeans.fit(pca_df)
-        if len(np.unique(kmeans.labels_)) <= numsamps - 1: 
-            score = silhouette_score(pca_df, kmeans.labels_)
-            silhouette_scores.append(score)
+        for k in range(2, kmax):
+            kmeans = KMeans(n_clusters = k, **kmeans_kwargs)
+            kmeans.fit(pca_df)
+            
+            if len(np.unique(kmeans.labels_)) <= numsamps - 1: 
+                score = silhouette_score(pca_df, kmeans.labels_)
+                silhouette_scores.append(score)
+                
+    elif clusttype == "hkmeans":
+        from sklearn.feature_extraction.text import TfidfVectorizer
+        from sklearn import metrics
+        from hkmeans import HKMeans
+        
+        for k in range(2, kmax):
+            hkmeans = HKMeans(n_clusters = k, random_state = 128, n_init = 10,
+                            n_jobs = 16, max_iter = 15)
+            hkmeans.fit(pca_df)
+            
+            if len(np.unique(hkmeans.labels_)) <= numsamps - 1: 
+                score = silhouette_score(pca_df, hkmeans.labels_)
+                silhouette_scores.append(score)
     
     return(silhouette_scores)
 
 
-def assign_to_cluster(pca_df, data_t, k):
+def assign_to_cluster(pca_df, data_t, k, clusttype):
     '''
     Function that assigns the samples to clusters and returns a data frame containing the cluster assignment for each sample
     
     Parameters 
     ----------
         pca_df : panda data frame
-            A df of PCA values output by pca_fit_transform
+            A df of PCA values that were calculated by pca_fit_transform
         data_t : panda data frame
             A transposed data frame, where samples are rows and variables (e.g. genes) are columns.
         k : int
             The value of k that corresponds to the maximum silhouette score
-    ''' 
-
-    kmeans_kwargs = {
-        "init": "random",
-        "n_init": 10,
-        "max_iter": 300,
-        "random_state": 42,
-    }
-    
-    km = KMeans(n_clusters=k, **kmeans_kwargs).fit(pca_df)
-
+        clusttype : str [choices are kmeans or hkmeans]
+            The type of clustering to perform. kmeans performs standard kmeans clustering while hkmeans uses the Hartigan method 
+    '''     
     cluster_map = pd.DataFrame()
     cluster_map['sample'] = data_t.index.values
-    cluster_map['cluster'] = km.labels_
+
+    if clusttype == "kmeans":
+        kmeans_kwargs = {
+            "init": "random",
+            "n_init": 10,
+            "max_iter": 300,
+            "random_state": 42,
+        }
+        
+        km = KMeans(n_clusters=k, **kmeans_kwargs).fit(pca_df)
+        cluster_map['cluster'] = km.labels_
+
+    elif clusttype == "hkmeans":
+        from sklearn.feature_extraction.text import TfidfVectorizer
+        from sklearn import metrics
+        from hkmeans import HKMeans
+        
+        hkmeans = HKMeans(n_clusters = k, random_state = 128, n_init = 10,
+                        n_jobs = 16, max_iter = 15)
+        hkmeans.fit(pca_df)
+        cluster_map['cluster'] = hkmeans.labels_
 
     print("\nBelow, the first column is the cluster number and the second column is the number of samples assigned to that cluster.")
     print(cluster_map['cluster'].value_counts())
     
     return(cluster_map)
-    # cluster_map.index = cluster_map['data_index']
-    # cluster_map['cluster'] = cluster_map['cluster'] + 1
-    # print(cluster_map['cluster'].value_counts())
--- network_analysis/clustering.py
+++ network_analysis/clustering.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     ArgGroup.add_argument("-d", "--datafile", type=str, help="Path to file containing the indegrees or expression data. If using expression data, file should not have a header and instead sample order should be supplied with --sampnames", required=True)
     ArgGroup.add_argument("-f", "--filetype", choices = ["csv", "txt"], type=str, help="File type of the input data file", required=True)
     ArgGroup.add_argument("-c", "--compnum", type=int, help="Maximum value of components to use in PCA. Must be less than or equal to number of samples", required=True)
+    ArgGroup.add_argument("-k", "--clusterchoice", choices = ["kmeans", "hkmeans"], type=str, help="Type of clustering to perform, either basic kmeans or kmeans using Hartigan's method (hkmeans)", required=True)
     ArgGroup.add_argument("-s", "--sampnames", type=str, help="Path to file containing the order of samples in the expression data", required=False)
     ArgGroup.add_argument("-o", "--outdir", type=str, help="Path to directory to output results to", required=True) 
     
@@ -47,39 +48,52 @@ if __name__ == '__main__':
     PCA_out = pca_fit_transform(data_scaled, args.compnum, data_t.index) 
     PCA_df = PCA_out["df"] 
     PCA_pcs = PCA_out["pcs"] 
+    
+    # Plot first two components
+    pca_df = pd.DataFrame()
+    pca_df["PC1"] = [x[0] for x in PCA_pcs]
+    pca_df["PC2"] = [x[1] for x in PCA_pcs]
 
-    # Choose a value for kmax. Normally we will use 11, but if user has 11 samples or less, this will not work so we need to adjust kmax 
-    if len(data.columns) <= 2:
-        raise Exception("Error: Must have at least 3 samples for finding clusters")
+    pca_outname = os.path.join(args.outdir, f"pca.png")
+
+    plt.scatter(pca_df["PC1"], pca_df["PC2"])
+    plt.savefig(pca_outname, bbox_inches='tight')
+
+    # Choose a value for kmax. Normally we will use 11, but if user has 11 samples or less, this will not work so we need to adjust kmax to be one less than the number of samples
+    if len(data.columns) <= 5:
+        raise Exception("Error: Must have at least 6 samples for finding clusters")
     elif len(data.columns) <= 10:
         kmax = len(data.columns) - 1
     else:
-        kmax = 10
-        
-    # Perform kmeans clustering
+        kmax = 11
+    
+    # Calculate silhouette scores to find optimal number of clusters
     print(f"Calculated kmax value to use in kmeans calculation: {kmax}")
-    sil_scores = km(PCA_pcs, kmax, len(data.columns))
-            
+    sil_scores = km(PCA_pcs, kmax, len(data.columns), args.clusterchoice)
+
     # Report results to user
     print(f"\nNumber of clusters : Silhouette score")
-    
     for i in range(2, len(sil_scores)+2):
         print(f"{i}: {sil_scores[i-2]:.2f}")
     
     largest_score = max(sil_scores)
     num_clusters = sil_scores.index(largest_score) + 2
-    
+
     if largest_score > 0.4:
-        print(f"\nThe largest silhouette score of {largest_score:.2f} was found for {num_clusters} clusters.")
+        print(f"\nThe largest silhouette score of {largest_score:.2f} was found for {num_clusters} clusters. Thus, cluster assignment will be performed for {num_clusters} clusters.")
     else:
-        print(f"\nThe largest silhouette score of {largest_score:.2f} was found for {num_clusters} clusters... Considering this value is smaller than 0.4, you may want to try alternative clustering methods.")
+        import warnings
+        warnings.warn("\nWARNING: Silhouette score is small!", RuntimeWarning)
+        print(f"The largest silhouette score of {largest_score:.2f} was found for {num_clusters} clusters... Considering this value is smaller than 0.4, you may want to try alternative clustering methods. Regardless, cluster assignment will be performed for {num_clusters} clusters.")
     
     # Assign samples to clusters based on the k value with highest silhouette score
-    assigned_samps = assign_to_cluster(PCA_pcs, data_t, num_clusters)
-
+    assigned_samps = assign_to_cluster(PCA_pcs, data_t, num_clusters, args.clusterchoice)
+    
+    # print("\nSample cluster assignment can be seen below.")
+    # print(assigned_samps.to_csv(sep='\t', index=False))
+    
     assigned_samps_fname = os.path.join(args.outdir, f"kmeans_sample_cluster_assignment_{num_clusters}_clusters.txt")
     assigned_samps.to_csv(assigned_samps_fname, sep = "\t", index = False)   
-    print(f"\nFile saved: {assigned_samps_fname}")     
         
     # Plot resulting silhouette scores
     plt.style.use("fivethirtyeight")
@@ -92,4 +106,6 @@ if __name__ == '__main__':
     outname = os.path.join(args.outdir, f"kmeans_silhouette_scores.png")
     plt.savefig(outname, bbox_inches='tight')
 
-    print(f"\nFile saved: {outname}")     
+    print(f"\nSample cluster assignment saved here: {assigned_samps_fname}")     
+    print(f"Calculated silhouette scores for each number of clusters saved here: {outname}")     
+    print(f"PCA plot for scaled input data saved here: {pca_outname}\n")     
