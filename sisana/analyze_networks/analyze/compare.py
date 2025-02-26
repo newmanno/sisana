Fixed the Mann-Whitney U value calculation

sisana/analyze_networks/analyze/compare.py
@@ -4,7 +4,7 @@ import scipy.stats
 import csv
 import re
 import pandas 
-from statistics import mean
+from statistics import mean, median
 import math
 import numpy as np
 import sys
@@ -33,7 +33,6 @@ def map_samples(mapfile, type1, type2):
             - type1: the name of the group in the first set of samples, must be present in the mapfile
             - type2: the name of the group in the second set of samples, must be present in the mapfile
     '''
-
     samp_type_dict = {}
 
     type1_list = []
@@ -73,106 +72,179 @@ def calc_tt(group1, group2, ttype):
             - ttype: str, the type of test to perform, either tt or mw
     '''
     
-    if ttype == 'tt':
-        p = stats.ttest_ind(group1, group2)       
-        statistic = p[0]
-        pval = p[1]
-    elif ttype == 'mw':
-        p = stats.mannwhitneyu(group1, group2)
-        
-        # Note that since mann whitney technically has two test statistics (one for U1 and the other for U2), we have to compute both and take the minimum.
-        # Wilcoxon also has two test statistics, but by default it returns the smaller of the two, so no need for SiSaNA to calculateNo
-        u1 = p[0]
-        u2 = group1.shape[0] * group2.shape[0] - p[0]
-        
-        statistic = min(u1,u2)
-        if mean(group1) < mean(group2):
-            statistic = statistic * -1
-
-        pval = p[1]
-    elif ttype == 'paired_tt':
-        p = stats.ttest_rel(group1, group2)
-        statistic = p[0]
-        pval = p[1]
-    elif ttype == 'wilcoxon':
-        p = stats.wilcoxon(group1, group2)
-        statistic = p[0]
-        pval = p[1]
-
-    return(statistic, pval)
+    # For testing purposes
+    from scipy.stats import mannwhitneyu 
+    # group1 = [1,2,5,7,9, 8,6,5,3]
+    # group2 = [2,3,6,8,9, 9,9,8,7]
     
-def transform_edge_to_positive_val(edgeval):
-    '''
-    Transform degree values to be positive so fold change can be calculated. This transformation 
-    is described in the paper "Regulatory Network of PD1 Signaling Is Associated with Prognosis 
-    in Glioblastoma Multiforme"
-        
-        Arguments:
-            - edgeval: float, value of edge
-            
-        Returns:
-            - float, transformed value of edge
-    '''
-    # We get an inf if we have too large of values, but since transforming a large value
-    # does not change the resulting value (i.e. ln(e^1000) + 1) = 1000, we can avoid the
-    # inf values by just not transforming those edges
-    if edgeval > 700:
-        newval = edgeval
-    else:
-        newval = math.log(np.exp(edgeval) + 1)
-    if newval == float("inf"):
-        raise Exception(f"val {edgeval} gives a result of inf")
-    return(newval)
+    if ttype != 'mw':
+        if ttype == 'tt':
+            p = stats.ttest_ind(group1, group2)                              
+        elif ttype == 'paired_tt':
+            p = stats.ttest_rel(group1, group2)
+        elif ttype == 'wilcoxon':
+            p = stats.wilcoxon(group1, group2)
 
-def calc_log2_fc(group1, group2):
+        statistic = p[0]
+        pval = p[1]
+        
+        return(statistic, pval)
+        
+    elif ttype == 'mw':
+        # p = stats.mannwhitneyu(group1, group2)
+        
+        # # Note that since mann whitney technically has two test statistics (one for U1 and the other for U2), we have to compute both and take the minimum.
+        # # Wilcoxon also has two test statistics, but by default it returns the smaller of the two, so no need for SiSaNA to calculate
+        # u1 = p[0]
+        # #u2 = group1.shape[0] * group2.shape[0] - p[0]
+        # u2 = len(group1) * len(group2) - p[0]
+        
+        # statistic = min(u1,u2)
+        # if mean(group1) < mean(group2):
+        #     statistic = statistic * -1
+
+        # pval = p[1]
+        
+        # print("\nValue of p, which is stats.mannwhitneyu(group1, group2):")
+        # print(p)
+        
+        # print("Value of pval, which is p[1]:")
+        # print(pval)
+        
+        # print("Value of statistic, which is the minimum of u1 (p[0]) and u2 (group1.shape[0] * group2.shape[0] - p[0]):")
+        # print(statistic)
+        
+        
+        
+        
+        #### The following is the previous code that (sort of) works. I have commented it out for testing ####
+        # from pingouin import mwu 
+        # mw_results = mwu(group1, group2)
+
+        # mw_results = mw_results.iloc[0, :].values.tolist()
+        # statistic, pval, cles = mw_results[0], mw_results[2], mw_results[4]    
+        # # statistic, pval, cles = ping_res['U-val'], ping_res['p-val'], ping_res['CLES']    
+
+        # if median(group1) < median(group2):
+        #     statistic = statistic * -1            
+        # # print(statistic)
+        # # print(pval) 
+        # # print(cles)
+        # return(statistic, pval, cles)
+        
+        
+        
+        
+        #### This is the new code I am testing ####
+        from pingouin import mwu
+        mw_results = mwu(group1, group2)
+        mw_results = mw_results.iloc[0, :].values.tolist()
+        mw_results_flipped = mwu(group2, group1)
+        mw_results_flipped = mw_results_flipped.iloc[0, :].values.tolist()
+        statistic_group1, pval, cles = mw_results[0], mw_results[2], mw_results[4]    
+        statistic_group2 = mw_results_flipped[0]
+        
+        mwu_min_ustat = min(statistic_group1, statistic_group2)
+
+        # if median(group1) < median(group2):
+        #     statistic = statistic * -1            
+
+        return(mwu_min_ustat, pval, cles)       
+        
+    
+def calc_group_difference(group1, group2, difftype=["mean", "median"]):
     '''
-    Calculates the log2 fold change of means across two groups
+    Calculates the difference of means across two groups
         
         Arguments:
             - group1: list of samples in first group
             - group2: list of samples in second group
+            - difftype: 
     '''
-    
-    # print("\n")
-    # print(f"group1 values:")
-    # print(group1)
-    # print(f"Mean of group 1: {scipy.mean(group1)}")
-    # print(f"group2 values:")
-    # print(group2)
-    # print(f"Mean of group 2: {scipy.mean(group2)}")
-
-    ### Note: For the following calculations, assuming the user has followed the previous SiSaNA steps,
-    ### neither group will ever have a mean of 0 since the preprocess.py step filters out any genes of 
-    ### low abundance. So to simplify the logic for this step, I have removed any checks for group means
-    ### of 0. May need to implement this change later though if people are not running preprocess.py.
-
-    # if scipy.mean(group1) == 0 and scipy.mean(group2) == 0:
-    #     log2FC = 0
-    # elif scipy.mean(group1) == 0:
+    if difftype == "mean":
+        return(mean(group2) - mean(group1))
+    elif difftype == "median":
+        return(median(group2) - median(group1))
         
-    # elif scipy.mean(group2) != 0:
+
+############################################################################
+### Note: The following functions were removed after determing that 
+###       calculating the log fold change in edges is not a good metric,
+###       since the edges are already logged to begin with. May rethink
+###       this calculation and re-add them later
+############################################################################
+# def transform_edge_to_positive_val(edgeval):
+#     '''
+#     Transform degree values to be positive so fold change can be calculated. This transformation 
+#     is described in the paper "Regulatory Network of PD1 Signaling Is Associated with Prognosis 
+#     in Glioblastoma Multiforme"
+        
+#         Arguments:
+#             - edgeval: float, value of edge
+            
+#         Returns:
+#             - float, transformed value of edge
+#     '''
+#     # We get an inf if we have too large of values, but since transforming a large value
+#     # does not change the resulting value (i.e. ln(e^1000) + 1) = 1000, we can avoid the
+#     # inf values by just not transforming those edges
+#     if edgeval > 700:
+#         newval = edgeval
+#     else:
+#         newval = math.log(np.exp(edgeval) + 1)
+#     if newval == float("inf"):
+#         raise Exception(f"val {edgeval} gives a result of inf")
+#     return(newval)
+
+# def calc_log2_fc(group1, group2):
+#     '''
+#     Calculates the log2 fold change of means across two groups
+        
+#         Arguments:
+#             - group1: list of samples in first group
+#             - group2: list of samples in second group
+#     '''
     
-    avg_g1 = mean(group1)
-    avg_g2 = mean(group2)
+#     # print("\n")
+#     # print(f"group1 values:")
+#     # print(group1)
+#     # print(f"Mean of group 1: {scipy.mean(group1)}")
+#     # print(f"group2 values:")
+#     # print(group2)
+#     # print(f"Mean of group 2: {scipy.mean(group2)}")
 
-    if avg_g1 == 0 or avg_g2 == 0:
-        log2FC = "NA"
-        # print("log2FC is NA")
+#     ### Note: For the following calculations, assuming the user has followed the previous SiSaNA steps,
+#     ### neither group will ever have a mean of 0 since the preprocess.py step filters out any genes of 
+#     ### low abundance. So to simplify the logic for this step, I have removed any checks for group means
+#     ### of 0. May need to implement this change later though if people are not running preprocess.py.
 
-    elif all(i >= 0 for i in group1) and all(i >= 0 for i in group2):
-        log2FC = math.log2(avg_g2/avg_g1)
-        # print(f"log2FC is {log2FC}\n")
-
-        # print(log2FC)
-    else:
-        print(group1, group2)
-        raise Exception("\n\nError: Negative values found in data. The log2 fold change can only be calculated on expression data. Negative values indicate that degree was likely used as an input type instead.\n")
-    # elif scipy.mean(group2) == 0:
-    #     # Take the smallest non-zero value, divide by 10, and use that as the average value for the denominator
-    #     no_zeros = [i for i in group1 if i != 0]
-    #     denom = min(no_zeros)/10
-    #     log2FC = math.log2(scipy.mean(group1)/denom)        
-    # else:
-    #     log2FC = "NA"
+#     # if scipy.mean(group1) == 0 and scipy.mean(group2) == 0:
+#     #     log2FC = 0
+#     # elif scipy.mean(group1) == 0:
+        
+#     # elif scipy.mean(group2) != 0:
     
-    return(log2FC)
+#     avg_g1 = mean(group1)
+#     avg_g2 = mean(group2)
+
+#     if avg_g1 == 0 or avg_g2 == 0:
+#         log2FC = "NA"
+#         # print("log2FC is NA")
+
+#     elif all(i >= 0 for i in group1) and all(i >= 0 for i in group2):
+#         log2FC = math.log2(avg_g2/avg_g1)
+#         # print(f"log2FC is {log2FC}\n")
+
+#         # print(log2FC)
+#     else:
+#         print(group1, group2)
+#         raise Exception("\n\nError: Negative values found in data. The log2 fold change can only be calculated on expression data. Negative values indicate that degree was likely used as an input type instead.\n")
+#     # elif scipy.mean(group2) == 0:
+#     #     # Take the smallest non-zero value, divide by 10, and use that as the average value for the denominator
+#     #     no_zeros = [i for i in group1 if i != 0]
+#     #     denom = min(no_zeros)/10
+#     #     log2FC = math.log2(scipy.mean(group1)/denom)        
+#     # else:
+#     #     log2FC = "NA"
+    
+#     return(log2FC)

sisana/analyze_networks/compare_groups.py
@@ -105,8 +105,29 @@ def compare_bw_groups(datafile: str, mapfile: str, datatype: str, groups: list, 
             # print(f"\nSamples in group 2:\n{groups["group2"]})")
             
             pval = compdf.apply(lambda row : calc_tt(row[groups["group1"]], row[groups["group2"]], testtype), axis = 1) 
+            
+        # Format the output data frame
+        pval_colname = testtype + "_pvalue"
+        pvaldf = pd.DataFrame({'Target':pval.index, pval_colname:pval.values})
+        newpvaldf = pd.DataFrame(pvaldf[pval_colname].to_list(), columns=['test_statistic',pval_colname])
+        newpvaldf['Target'] = pval.index
+        newpvaldf = newpvaldf.set_index('Target')
+        test_stat_column = "test_statistic"
+         
     else:
-        mwu_uval, mwu_pval, mwu_cles = compdf.apply(lambda row : calc_tt(row[sampdict[groups[1]]], row[sampdict[groups[0]]], testtype), axis = 1)
+        compdf.apply(lambda row : calc_tt(row[sampdict[groups[1]]], row[sampdict[groups[0]]], testtype), axis = 1)
+        # mwu_uval, mwu_pval, mwu_cles = compdf.apply(lambda row : calc_tt(row[sampdict[groups[1]]], row[sampdict[groups[0]]], testtype), axis = 1)
+        mwu_calculations = compdf.apply(lambda row : calc_tt(row[sampdict[groups[1]]], row[sampdict[groups[0]]], testtype), axis = 1)
+    
+        # Format the output data frame
+        pval_colname = testtype + "_pvalue"
+        pvaldf = pd.DataFrame({'Target':mwu_calculations.index, pval_colname:mwu_calculations})
+        newpvaldf = pd.DataFrame(pvaldf[pval_colname].to_list(), columns=['mw_uvalue', 'mw_pvalue', 'mw_CLES'])
+        newpvaldf['Target'] = mwu_calculations.index
+        newpvaldf = newpvaldf.set_index('Target')
+        pval_colname = "mw_pvalue"
+        test_stat_column = "mw_uvalue"
+
     # For expression, do regular log2FC
     # For degrees, first need to transform the edge value by doing ln(e^w + 1),
     # then calculate degrees. Then you can do the log2FC of degrees
@@ -117,23 +138,12 @@ def compare_bw_groups(datafile: str, mapfile: str, datatype: str, groups: list, 
     #     fc = compdf.apply(lambda row : calc_log2_fc(row[sampdict[groups[1]]], row[sampdict[groups[0]]]), axis = 1)
 
     # if datatype == "degree":
-    print(mwu_uval)
-    sys.exit(1)
+    # print(mwu_uval)
    
     mean_diff = compdf.apply(lambda row : calc_group_difference(row[sampdict[groups[0]]], row[sampdict[groups[1]]], difftype="mean"), axis = 1)
     median_diff = compdf.apply(lambda row : calc_group_difference(row[sampdict[groups[0]]], row[sampdict[groups[1]]], difftype="median"), axis = 1)
 
-    print("Comparisons finished...")
-    
-    # Format the output data frame
-    pval_colname = testtype + "_pvalue"
-
-    pvaldf = pd.DataFrame({'Target':pval.index, pval_colname:pval.values})
-
-    newpvaldf = pd.DataFrame(pvaldf[pval_colname].to_list(), columns=['test_statistic',pval_colname])
-
-    newpvaldf['Target'] = pval.index
-    newpvaldf = newpvaldf.set_index('Target')  
+    print("Comparisons finished...") 
     
     # Calcuate means per group
     # if args.datatype == "expression":            
@@ -141,26 +151,36 @@ def compare_bw_groups(datafile: str, mapfile: str, datatype: str, groups: list, 
     mean_g1_colname = f"mean_{groups[0]}"      
     newpvaldf[mean_g2_colname] = compdf[sampdict[groups[1]]].mean(axis=1)
     newpvaldf[mean_g1_colname] = compdf[sampdict[groups[0]]].mean(axis=1)
-    
     meandiff_colname = f"difference_of_means_({groups[1]}-{groups[0]})"      
     newpvaldf[meandiff_colname] = mean_diff
     newpvaldf["abs(difference_of_means)"] = abs(mean_diff)
-    
-    mediandiff_colname = f"difference_of_means_({groups[1]}-{groups[0]})"      
+
+    # Calcuate medians per group    
+    median_g2_colname = f"median_{groups[1]}"    
+    median_g1_colname = f"median_{groups[0]}"      
+    newpvaldf[median_g2_colname] = compdf[sampdict[groups[1]]].median(axis=1)
+    newpvaldf[median_g1_colname] = compdf[sampdict[groups[0]]].median(axis=1)    
+    mediandiff_colname = f"difference_of_medians_({groups[1]}-{groups[0]})"      
     newpvaldf[mediandiff_colname] = median_diff
     newpvaldf["abs(difference_of_medians)"] = abs(median_diff)
 
     # Perform multiple test correction
+    
     newpvaldf['FDR'] = stats.false_discovery_control(newpvaldf[pval_colname])
     newpvaldf = newpvaldf.sort_values(pval_colname, ascending = True)
-   
+    print(newpvaldf)
+
     # Create new df without pval, ranked on test statistic
-    ranked = newpvaldf.sort_values('test_statistic', ascending = False)
+    ranked = newpvaldf.sort_values(test_stat_column, ascending = False)
     ranked.drop([pval_colname, 'FDR'], inplace=True, axis=1)
-    ranked = ranked["test_statistic"]
+    ranked = ranked[test_stat_column]
     
-    if testtype == "mw":
-        newpvaldf.rename(columns={"test_statistic": "U-val"})
+    # Rearrange column order so that FDR calculations comes after p-value
+    newpvaldf = newpvaldf.iloc[:, [0, 1, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+    print(newpvaldf)
+
+    # if testtype == "mw":
+    #     newpvaldf.rename(columns={"test_statistic": "U-val"})
     
     # Write to disk
     if testtype == "tt" or testtype == "mw":
@@ -173,7 +193,10 @@ def compare_bw_groups(datafile: str, mapfile: str, datatype: str, groups: list, 
     # Make output directory if it does not already exist
     Path(outdir).mkdir(parents=True, exist_ok=True)    
     
-    newpvaldf.to_csv(save_file_path, sep = "\t")
+    # Remove the abs() columns (previously for sorting) for exporting the file
+    columns_to_keep = [x for x in newpvaldf.columns if x[0:3] != "abs"]
+    
+    newpvaldf.to_csv(save_file_path, columns = columns_to_keep, sep = "\t")
     ranked.to_csv(save_file_path_ranked, sep = "\t", header = False)
 
     print(f"\nFile saved: {save_file_path}\nThis file contains all the statistics results and is just for your reference.\n")