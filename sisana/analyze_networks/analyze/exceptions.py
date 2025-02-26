68d2da4522bb4059baeb099e6f00388a833033d1
newmanno <newmanno@oregonstate.edu>
2025-02-26 14:54:06 +0100

    Added IncorrectHeaderError

--- sisana/analyze_networks/analyze/exceptions.py
+++ sisana/analyze_networks/analyze/exceptions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class NotASubsetError(Exception):
     """
     Raise when the supplied list (genes/samples) is not a subset of the items in the data frame
@@ -9,7 +11,7 @@ class NotASubsetError(Exception):
         message -- explanation of the error
     """
 
-    def __init__(self, user_list, data_list, dtype, message=f"Error: The genes in the supplied list are not a subset of the genes in the dataset."):
+    def __init__(self, user_list, data_list, dtype, message="Error: The genes in the supplied list are not a subset of the genes in the dataset."):
         
         self.user_list = user_list
         self.data_list = data_list
@@ -27,3 +29,16 @@ class NotASubsetError(Exception):
         self.message = message 
         super().__init__(self.message)
         
+class IncorrectHeaderError(Exception):
+    """
+    Raise when the user's metadata file header is not in the correct format of "name,group"
+    
+    Attributes:
+        metadf : Data frame of the user's metadata file
+        message -- explanation of the error
+    """
+    def __init__(self, metadf=pd.DataFrame, message="Error: The header of the metadata file must be in the format 'name,group'."):
+        self.metadf = metadf
+        print(f"\nThe header of your metadata file looks like the following:\n {self.metadf.head()} \n")
+        self.message = message 
+        super().__init__(self.message)
