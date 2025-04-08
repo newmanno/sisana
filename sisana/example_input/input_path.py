c95343a15c6aabea237b98f95efea23dbb44445b
newmanno <newmanno@oregonstate.edu>
2025-04-08 15:21:03 +0200

    Updated with 200 LumALumB sample files

--- sisana/example_input/input_path.py
+++ sisana/example_input/input_path.py
@@ -14,17 +14,22 @@ def find_example_paths():
         - list of paths to be copied in main sisana script
     '''
     
-    fnames = ["BRCA_TCGA_20_LumA_LumB_samps_5000_genes_exp.tsv",
+    fnames = ["BRCA_TCGA_200_LumA_LumB_samps_mapping.csv",
+              "BRCA_TCGA_200_LumA_LumB_samps_mapping_w_header.csv",
+              "BRCA_TCGA_200_LumA_LumB_samps_survival_data.csv",
+              "BRCA_TCGA_20_LumA_LumB_samps_5000_genes_exp.tsv",
               "BRCA_TCGA_20_LumA_LumB_samps_mapping.csv",
-              "BRCA_TCGA_20_LumA_LumB_samps_survival_data.csv",
-              "genes.txt",
+              "c2.cp.kegg_medicus.v2023.2.Hs.symbols.gmt",
+              "c2.cp.reactome.v2023.2.Hs.symbols.gmt",
+              "clustermap_genes.txt",
+              "Hallmark.v2023.2.Hs.symbols.gmt",
+              "lioness_df_indegree_3_decimal_places_subset_200_LumALumB_samps.csv",
               "motif_tcga_brca.tsv",
               "params.yml",
               "ppi_tcga_brca.tsv",
-              "c2.cp.kegg_medicus.v2023.2.Hs.symbols.gmt",
-              "c2.cp.reactome.v2023.2.Hs.symbols.gmt",
-              "Hallmark.v2023.2.Hs.symbols.gmt",
-              "top_50_genes_heatmap.txt"]
+              "quantity_plot_genes.txt",
+              "top_50_genes_heatmap.txt",
+              "volcano_plot_genes.txt"]
     dir_path = os.path.dirname(os.path.realpath(__file__))
     files = [dir_path + "/" + x for x in fnames]
     return(files)
