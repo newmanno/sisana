b6e5a87b70d8a57994798411dc28d1865260d3b8
newmanno <newmanno@oregonstate.edu>
2025-05-30 13:27:19 +0200

    Changed input files

--- sisana/example_input/input_path.py
+++ sisana/example_input/input_path.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def find_example_paths():
     '''
@@ -14,21 +15,19 @@ def find_example_paths():
         - list of paths to be copied in main sisana script
     '''
     
-    fnames = ["BRCA_TCGA_200_LumA_LumB_samps_mapping.csv",
-              "BRCA_TCGA_200_LumA_LumB_samps_mapping_w_header.csv",
+    fnames = ["BRCA_TCGA_200_LumA_LumB_samps_mapping_w_header.csv",
               "BRCA_TCGA_200_LumA_LumB_samps_survival_data.csv",
               "BRCA_TCGA_20_LumA_LumB_samps_5000_genes_exp.tsv",
-              "BRCA_TCGA_20_LumA_LumB_samps_mapping.csv",
               "c2.cp.kegg_medicus.v2023.2.Hs.symbols.gmt",
               "c2.cp.reactome.v2023.2.Hs.symbols.gmt",
-              "clustermap_genes.txt",
+              "genes_to_extract.txt",
+              "heatmap_genes.txt",
               "Hallmark.v2023.2.Hs.symbols.gmt",
               "lioness_df_indegree_3_decimal_places_subset_200_LumALumB_samps.csv",
               "motif_tcga_brca.tsv",
               "params.yml",
               "ppi_tcga_brca.tsv",
               "quantity_plot_genes.txt",
-              "top_50_genes_heatmap.txt",
               "volcano_plot_genes.txt"]
     dir_path = os.path.dirname(os.path.realpath(__file__))
     files = [dir_path + "/" + x for x in fnames]
