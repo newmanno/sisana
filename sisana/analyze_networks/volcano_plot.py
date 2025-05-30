bb98f8ec215bd3e323a6d16ecee66fb096525f38
newmanno <newmanno@oregonstate.edu>
2025-05-30 13:00:28 +0200

    Added user-defined x-axis threshold

--- sisana/analyze_networks/volcano_plot.py
+++ sisana/analyze_networks/volcano_plot.py
@@ -9,8 +9,8 @@ import os
 from .analyze import file_to_list, filter_for_top_genes, filter_for_user_defined_genes, create_label_list
 import warnings
 
-def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: str,
-                 outdir: str, genelist: list=[], difftype: str="mean", top: bool=True, numlabels: int=15):
+def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: str, xaxisthreshold: float,  difftype: str,
+                 outdir: str, top: bool=True, numlabels: int=15, genelist: str=""):
     """
     Description:
         This code performs a survival analysis between two user-defined groups and outputs
@@ -23,7 +23,7 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
         - fccol: str, The name of the column containing the difference in medians or means
         - adjpcol: str, The name of the column containing the adj. p-value
         - adjpvalthreshold: str, Threshold to use for the adjusted p-value
-        - genelist: str, List of genes to plot. Alternatively, the top X genes can be plotted instead if top=True.
+        - genelist: str, Path to a .txt file containing a list of genes to plot. Alternatively, the top {numlabels} genes can be plotted instead if top=True.
         - outdir: str, Path to directory to output file to
         - difftype: str, The type of difference to use for the x-axis. "mean" will be difference in means and "median"
           refers to difference in medians
@@ -45,6 +45,7 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
     # ArgGroup.add_argument("-t", "--topvalstoplot", type=int, help="Number of top values to label", required=True)   
     # ArgGroup.add_argument("-o", "--outdir", type=str, help="Path to directory to output file to", required=True) 
     
+    
     # Create output directory if one does not already exist
     os.makedirs(outdir, exist_ok=True)
     
@@ -55,9 +56,9 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
     plt.figure(figsize=(6, 6), dpi = 1200) 
     plt.scatter(x=stats[diffcol],y=stats[adjpcol].apply(lambda x:-np.log10(x)), s=1)
 
-    down = stats[(stats[diffcol] <= 0) & (stats[adjpcol] <= adjpvalthreshold)]
-    up = stats[(stats[diffcol] > 0) & (stats[adjpcol] <= adjpvalthreshold)]
-    # not_down_or_up = stats[(stats[diffcol] < fcthreshold) & (stats[fccol] > -1 * fcthreshold) & (stats[adjpcol] < adjpvalthreshold)]
+    down = stats[(stats[diffcol] <= xaxisthreshold) & (stats[adjpcol] <= adjpvalthreshold)]
+    up = stats[(stats[diffcol] > xaxisthreshold) & (stats[adjpcol] <= adjpvalthreshold)]
+    not_down_or_up = stats[(stats[diffcol] < xaxisthreshold) & (stats[diffcol] > -1 * xaxisthreshold) & (stats[adjpcol] < adjpvalthreshold)]
     notsig = stats[stats[adjpcol] > adjpvalthreshold]
     
     if len(down) + len(up) == 0:
@@ -66,26 +67,27 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
     plt.scatter(x=down[diffcol], y=down[adjpcol].apply(lambda x:-np.log10(x)), s=3, label="Down-regulated",color="blue")
     plt.scatter(x=up[diffcol], y=up[adjpcol].apply(lambda x:-np.log10(x)), s=3, label="Up-regulated",color="orange")
     plt.scatter(x=notsig[diffcol], y=notsig[adjpcol].apply(lambda x:-np.log10(x)), s=3, label="Not significant",color="gainsboro")
-    # plt.scatter(x=not_down_or_up[diffcol], y=not_down_or_up[adjpcol].apply(lambda x:-np.log10(x)), s=3, color="darkgrey")
+    plt.scatter(x=not_down_or_up[diffcol], y=not_down_or_up[adjpcol].apply(lambda x:-np.log10(x)), s=3, color="darkgrey")
 
     # Label the user-defined genes if given, otherwise plot just the top genes
-    if not top:
-            # Find the overlap in the user gene list and the up/down genes 
-            genelist = file_to_list(genelist)
-            
-            user_defined_upgenes = list(set(genelist).intersection(up.index))
-            user_defined_dngenes = list(set(genelist).intersection(down.index))
-            
-            if len(user_defined_upgenes) + len(user_defined_dngenes) != len(genelist):
-                warnings.warn("\nWarning: Some genes in the input list are not significant and will not be labeled.")
+    if genelist != None:
+        # Find the overlap in the user gene list and the up/down genes 
+        genelist = file_to_list(genelist)
+        
+        user_defined_upgenes = list(set(genelist).intersection(up.index))
+        user_defined_dngenes = list(set(genelist).intersection(down.index))
+        
+        if len(user_defined_upgenes) + len(user_defined_dngenes) != len(genelist):
+            warnings.warn("\nWarning: Some genes in the input list are not significant and will not be labeled.")
 
-            up_subset = filter_for_user_defined_genes(datafile=up, genes=user_defined_upgenes, verbose=False)
-            dn_subset = filter_for_user_defined_genes(datafile=down, genes=user_defined_dngenes, verbose=False)
-            
-            uptexts = create_label_list(statsfile=up_subset, difference_column=diffcol, adjp_column=adjpcol)
-            dntexts = create_label_list(statsfile=dn_subset, difference_column=diffcol, adjp_column=adjpcol)
+        up_subset = filter_for_user_defined_genes(datafile=up, genes=user_defined_upgenes, verbose=False)
+        dn_subset = filter_for_user_defined_genes(datafile=down, genes=user_defined_dngenes, verbose=False)
+        
+        uptexts = create_label_list(statsfile=up_subset, difference_column=diffcol, adjp_column=adjpcol)
+        dntexts = create_label_list(statsfile=dn_subset, difference_column=diffcol, adjp_column=adjpcol)
 
-    else:
+    else: # Plot the top genes
+        print("No list of genes to label was given, so labeling the top genes instead. Adjust the 'numlabels' parameter to change the number of genes labeled.")
         if len(up) >= numlabels:
             up_subset = up.sort_values(adjpcol, ascending=True).head(numlabels)
         else:
@@ -96,11 +98,15 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
 
         if len(down) >= numlabels:
             dn_subset = down.sort_values(adjpcol, ascending=True).head(numlabels)   
+            print(dn_subset)
         else:
             warnings.warn("Warning: There are not enough significant 'down' values that pass the threshold. Only those that pass will be labeled.")
             dn_subset = down.sort_values(adjpcol, ascending=True).head(len(up))
             
         dntexts = create_label_list(statsfile=dn_subset, difference_column=diffcol, adjp_column=adjpcol)
+        # print(dntexts)
+        # sys.exit(0)
+
 
     # Add labels to plot
     adjust_text(uptexts,arrowprops=dict(arrowstyle="-", color='black', lw=0.5))        
@@ -108,13 +114,16 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
     
     difftype= "median"
     if difftype == "mean":
-        plt.xlabel("Difference in means")
+        plt.xlabel("Difference in mean degree")
     elif difftype == "median":
-        plt.xlabel("Difference in medians")
+        plt.xlabel("Difference in median degree")
 
     plt.ylabel("-log10(FDR)")
-    # plt.axvline(-1 * fcthreshold, color="grey", linestyle="--")
-    # plt.axvline(fcthreshold, color="grey", linestyle="--")
+
+    fcthreshold = 50
+    plt.axvline(-1 * fcthreshold, color="grey", linestyle="--")
+    plt.axvline(fcthreshold, color="grey", linestyle="--")
+
     plt.axhline(-np.log10(adjpvalthreshold), color="grey", linestyle="--")
     plt.legend()
     
